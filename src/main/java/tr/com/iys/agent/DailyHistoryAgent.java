1ae0c3cbf14071c4e26024d0f05e22f4f889c75c
Oguzhan <oguzhan@Oguzhans-MacBook-Pro-2.local>
2020-09-02 20:20:44 +0300

    initial commit 3

--- src/main/java/tr/com/iys/agent/DailyHistoryAgent.java
+++ src/main/java/tr/com/iys/agent/DailyHistoryAgent.java
@@ -20,65 +20,83 @@ import com.google.gson.JsonObject;
 
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class DailyHistoryAgent {
-	String pulsarEndpoint = "pulsar://" + "localhost" + ":"
-			+ "6650";
+	String pulsarEndpoint = "pulsar://" + "localhost" + ":" + "6650";
 
-	String pulsarAdminEndpoint = "http://" + "localhost" + ":"
-			+ "8082";
+	String pulsarAdminEndpoint = "http://" + "localhost" + ":" + "8082";
 	private static final String ELASTIC_HOST = "localhost";
 	private static final int ELASTIC_PORT = 9200;
 	private IysPulsarClient iysPulsarClient = null;
-	private ElasticClient elasticClient = null; 
-	
+	private ElasticClient elasticClient = null;
+
 	public static final String dailyHistoryTopic = "iys.module.dailyhistoryagent.action.add-daily-history";
 	public static final String DAILY_HISTORY_SUBSCRIPTION = "daily-history-subscription";
-	
+
 	private Consumer<?> dailyHistoryConsumer = null;
-	
+
+	private List<Consumer<?>> consumerList = new ArrayList<>();
+
 	public static void main(String[] args) {
 		DailyHistoryAgent dh = new DailyHistoryAgent();
 		dh.init();
-		
-		try {
-			
-			Flowable.range(0, 10).flatMap(id -> dh.getMessagesAsync(id).subscribeOn(Schedulers.io())).subscribe();
-			
-			//Thread.sleep(1000000L);
 
+		try {
+
+			Flowable<List<Messages<?>>> repeat = Flowable.range(0, 10)
+					.flatMap(id -> dh.getMessagesAsync(id).subscribeOn(Schedulers.computation())).buffer(10)
+					.repeatUntil(() -> {
+						return false;
+					});
+			Disposable subscribe = repeat.subscribe(foo -> {
+				System.out.println("clazz: " + foo.size());
+				List<JsonObject> list = new ArrayList<>();
+				for (Messages<?> msgs : foo) {
+					for (Message<?> message : msgs) {
+						JsonObject messageObject = dh.getMessageObject(message);
+						System.out.println(messageObject.get("messageId").getAsString());
+						// System.out.println(Thread.currentThread()+" msg seq:
+						// "+messageObject.get("messageId").getAsString()+" rx id: "+id);
+					}
+				}
+			});
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	
-	private  void init() {
-		//TODO pulsar consume
+
+	private void init() {
+		// TODO pulsar consume
 		JsonObject elasticProperties = new JsonObject();
 		this.iysPulsarClient = PulsarInstance.getIysPulsarClient(pulsarEndpoint, pulsarAdminEndpoint, 0);
-		//this.elasticClient = ElasticInstance.getClient(ELASTIC_HOST, ELASTIC_PORT, new HashMap<String,String>(), 1,elasticProperties);
-		
-		this.dailyHistoryConsumer = iysPulsarClient.createPulsarConsumer(dailyHistoryTopic, DAILY_HISTORY_SUBSCRIPTION, 100);
-	}
-	
-	private Flowable<Messages<?>> getMessagesAsync(int id) throws Exception {
-		  return Flowable.fromCallable(() -> {return getMessages(id);});
-	}
-	
-	private Messages<?> getMessages(int id) throws PulsarClientException {
-		//Messages<?> batchReceive = this.dailyHistoryConsumer.batchReceive();
-		//TODO ???
-		 Messages<?> msgs = this.dailyHistoryConsumer.batchReceive();
-		 for (Message<?> message : msgs) {
-			 JsonObject messageObject = getMessageObject(message);
-			System.out.println(Thread.currentThread()+" msg seq: "+messageObject.get("messageId").getAsString()+" rx id: "+id);
+		for (int i = 0; i < 10; i++) {
+			Consumer<?> tempConsumer = iysPulsarClient.createPulsarConsumer(dailyHistoryTopic,
+					DAILY_HISTORY_SUBSCRIPTION, 100);
+			consumerList.add(tempConsumer);
 		}
-		 return msgs;
+//		this.elasticClient = ElasticInstance.getClient(ELASTIC_HOST, ELASTIC_PORT, new HashMap<String,String>(), 1,elasticProperties);
+
+		//
 	}
-	
+
+	private Flowable<Messages<?>> getMessagesAsync(int id) throws Exception {
+		return Flowable.fromCallable(() -> {
+			return getMessages(id);
+		});
+	}
+
+	private Messages<?> getMessages(int id) throws PulsarClientException {
+		// Messages<?> batchReceive = this.dailyHistoryConsumer.batchReceive();
+		// TODO ???
+		System.out.println("Getting messages with requester id: " + id);
+		Messages<?> msgs = this.consumerList.get(id).batchReceive();
+		System.out.println("Getting messages with requester id: " + id + " msg count: " + msgs.size());
+		return msgs;
+	}
+
 	private JsonObject getMessageObject(Message<?> msg) {
 		JsonObject json = null;
 		try {
@@ -88,5 +106,5 @@ public class DailyHistoryAgent {
 		}
 		return json;
 	}
-	
+
 }
